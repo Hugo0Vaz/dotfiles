639567cf06cc4af1a50966067b5accbd8cf77fbf
Hugo Vaz <hugomartinsvaz@gmail.com>
2024-05-01 22:53:25 -0300

    adicionando lualine e alguns temas

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -11,12 +11,12 @@ return {
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
 
-    local statusline = require 'mini.statusline'
-    statusline.setup()
-
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
+    -- local statusline = require 'mini.statusline'
+    -- statusline.setup()
+    --
+    -- ---@diagnostic disable-next-line: duplicate-set-field
+    -- statusline.section_location = function()
+    --   return '%2l:%-2v'
+    -- end
   end,
 }
