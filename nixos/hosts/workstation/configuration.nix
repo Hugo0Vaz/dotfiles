12f9a50c0b72e429f5338b1d5f76cd17cccc128f
Hugo Martins Vaz Silva <Hugo0Vaz>
2024-07-02 16:03:53 -0300

    configurando minha nixos workstation

--- nixos/hosts/workstation/configuration.nix
+++ nixos/hosts/workstation/configuration.nix
@@ -1,38 +1,28 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, options, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos-workstations";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.networkmanager.enable = true; 
+  networking.timeServers = options.networking.timeServers.default
+    ++ [ "a.ntp.br" ];
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
+
   services.geoclue2.enable = true;
-  networking.timeServers = options.networking.timeServers.default ++ [ "a.ntp.br" ]; 
   services.ntp.enable = true;
   services.timesyncd.enable = true;
-  # Select internationalisation properties.
+
   i18n.defaultLocale = "pt_BR.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -47,28 +37,21 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  # Configure keymap in X11
   services.xserver = {
     layout = "br";
     xkbVariant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "br-abnt2";
   services.localtimed.enable = true;
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -76,64 +59,51 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.hugomvs = {
     isNormalUser = true;
     description = "Hugo Martins Vaz Silva";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    	kdePackages.kate
-    #  thunderbird
-    ];
+    packages = with pkgs;
+      [
+        kdePackages.kate
+      ];
   };
 
-  # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  	vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  	wget
-	google-chrome
-	obsidian
-	neovim
-	wezterm
-	git
-	stow
-	gcc
-	go
-	php
-	lua
-	nodejs
-	python3
-	kotlin
-	openjdk
-	php83Packages.composer
-	vscode
-	whatsapp-for-linux
-	zip
-	unzip
-	lua-language-server
-	zsh
-	fish
-	oh-my-zsh
-	fzf
-	fzf-zsh
+    vim
+    wget
+    google-chrome
+    obsidian
+    neovim
+    wezterm
+    git
+    stow
+    gcc
+    go
+    php
+    lua
+    nodejs
+    python3
+    kotlin
+    openjdk
+    php83Packages.composer
+    vscode
+    whatsapp-for-linux
+    zip
+    unzip
+    lua-language-server
+    zsh
+    fish
+    oh-my-zsh
+    fzf
+    fzf-zsh
+    nixfmt-classic
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
