d31c815e2964e5a4c84dbb044f149b3737c32922
Hugo Martins Vaz Silva <Hugo0Vaz>
2024-07-02 17:53:34 -0300

    changing terminal font size:w

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.default_prog = { "bash" }
+config.font_size = 10.0
 
 config.color_scheme = "Gruvbox Dark (Gogh)"
 return config
