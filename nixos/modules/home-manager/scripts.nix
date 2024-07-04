435bb45fc408df06f8b6677443dc5a6d507c6c16
Hugo Martins Vaz Silva <Hugo0Vaz>
2024-07-04 13:29:06 -0300

    removendo a configuração do nixos do repositório de dotfiles

--- nixos/modules/home-manager/scripts.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, options, ... }:
-
-let
-  helloWorld = pkgs.writeShellScriptBin "helloWorld" ''
-    echo Hello World
-  '';
-
-in { home = { environment.systemPackages = [ helloWorld ]; }; }
